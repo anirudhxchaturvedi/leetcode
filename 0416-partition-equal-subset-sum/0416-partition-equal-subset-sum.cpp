b917bf88480e991d9f5b804048de4e9665b5c3e4
Anirudh Chaturvedi <98817097+anirudhxchaturvedi@users.noreply.github.com>
2024-06-23 02:06:02 +0530

    Time: 90 ms (80.3%), Space: 99.4 MB (18.55%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    bool subsetSum(vector<int>& nums, int sum) {
-        int n = nums.size();
-        vector<int> dp(sum + 1, 0);
-        dp[0] = 1;  // There's always a way to get sum 0, by picking no elements
-
-        for (int i = 0; i < n; i++) {
-            for (int j = sum; j >= nums[i]; j--) {
-                dp[j] = dp[j] || dp[j - nums[i]];
+    bool subsetSum(vector<int>nums, int sum)
+    {
+        int n=nums.size();
+        vector<vector<int>>dp(n+1,vector<int>(sum+1,0));
+        for(int i=0;i<n+1;i++)
+        {
+            dp[i][0]=1;
+        }
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=0;j<sum+1;j++){
+                if(nums[i-1]<=j){
+                    dp[i][j]=dp[i-1][j-nums[i-1]]||dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
             }
         }
-
-        return dp[sum];
+        return dp[n][sum];
     }
-
     bool canPartition(vector<int>& nums) {
-        int sum = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            sum += nums[i];
-        }
-
-        // If the total sum is odd, it's not possible to partition into two equal subsets
-        if (sum % 2 != 0) {
-            return false;
-        }
-
-        // Check if there's a subset with sum equal to half of total sum
-        return subsetSum(nums, sum / 2);
+        int sum=0;
+        for (int i=0;i<nums.size();i++)
+        sum+=nums[i];
+        if(sum%2!=0)
+        return false;
+            return subsetSum(nums,sum/2);
     }
 };
